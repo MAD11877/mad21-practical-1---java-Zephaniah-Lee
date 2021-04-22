0b59a3c2c81cde3f3102ce4f687cca4cfeea2249
PinZephaniahZep <>
2021-04-22 01:56:42 +0000

    Completed Question 3

--- Question3.java
+++ Question3.java
@@ -11,6 +11,8 @@ public class Question3
      */
      
     Scanner in = new Scanner(System.in);
-    
+    System.out.print("Enter Integer: ");
+    int integer = in.nextInt();
+    System.out.println(integer*integer);
   }
 }
